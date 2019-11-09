38b846a395bd49ae25e41bd911aacac9816f87e6
Your Name <youremail@domain.com>
2019-11-09 23:39:04 +0300

    we instructed our program to call livetime method every second and now the app can display time refreshed per second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function liveTime() {
+
 var greeting = React.createElement('h1', {}, 'Hello, World!');
-var clock = React.createElement('h2', {}, 'It is ${new Date().toLocaleTimeString()}');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
