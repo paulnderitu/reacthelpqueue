5333673410dacf43cd692aefa0d956fa3c8b5c6e
Your Name <youremail@domain.com>
2019-11-09 23:09:11 +0300

    nested code in a js liveTime method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+function liveTime() {
 var greeting = React.createElement('h1', {}, 'Hello, World!');
 var clock = React.createElement('h2', {}, 'It is ${new Date().toLocaleTimeString()}');
 var app = React.createElement('div', {}, greeting, clock);
@@ -6,3 +7,4 @@ ReactDOM.render(
   app,
   document.getElementById('react-app-root')
 );
+}
